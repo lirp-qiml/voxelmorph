4272290267d51a865cf0a225666ba8fd21e50bb3
rgeng5 <72523017+rgeng5@users.noreply.github.com>
2022-05-02 09:17:11 -0500

    Update losses.py

--- voxelmorph/torch/losses.py
+++ voxelmorph/torch/losses.py
@@ -103,7 +103,13 @@ class Grad:
         dy = torch.abs(y_pred[:, :, 1:, :, :] - y_pred[:, :, :-1, :, :])
         dx = torch.abs(y_pred[:, :, :, 1:, :] - y_pred[:, :, :, :-1, :])
         dz = torch.abs(y_pred[:, :, :, :, 1:] - y_pred[:, :, :, :, :-1])
-
+        
+        if self.penalty == 'TV':
+            grad = torch.sqrt(dx*dx + dy*dy + dz*dz)
+            if self.loss_mult is not None:
+                grad *= self.loss_mult
+            return grad
+    
         if self.penalty == 'l2':
             dy = dy * dy
             dx = dx * dx
